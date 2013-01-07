20959b1415b607bd7cde97ff2b120726b635f2b4
Juan M <jm@guide42.com>
2013-01-07 15:58:39 +0100

    [Examples] More simple.

--- examples/basic.php
+++ examples/basic.php
@@ -15,29 +15,19 @@ $ncurses->cursorVisibility(false);
 // Create a Window the same size of the Screen
 $window = new Window(new Rect(0, 0, $ncurses->rows, $ncurses->cols));
 
-// Create a center Hello World label
 $helloWorld = new Widget\Label('Hello World', new Rect(0, 0));
-$helloWorld->rect->center = $window->rect->center;
 $helloWorld->bold = true;
-$window->widgets->add($helloWorld);
 
-// Create person
-$person = new Widget\Text(" O\n/|\\\n ^", new Rect(1, 1));
-$window->widgets->add($person);
+$description = new Widget\Label('. Press Q to exit.', new Rect(0, 11));
+
+$window->widgets->add($helloWorld);
+$window->widgets->add($description);
 
 while (true) {
     foreach ($ncurses->events as $event) {
         if ($event->type === Event::TYPE_KEY) {
             if ($event->key === 113) {
                 return;
-            } elseif ($event->key === NCURSES_KEY_LEFT) {
-                $person->rect->left -= 1;
-            } elseif ($event->key === NCURSES_KEY_RIGHT) {
-                $person->rect->left += 1;
-            } elseif ($event->key === NCURSES_KEY_DOWN) {
-                $person->rect->top += 1;
-            } elseif ($event->key === NCURSES_KEY_UP) {
-                $person->rect->top -= 1;
             }
         }
     }
--- examples/text.php
+++ examples/text.php
@@ -9,31 +9,24 @@ use Ncurses\Window;
 use Ncurses\Widget;
 
 $text = <<<EOF
-This document is an introduction to programming with curses. It is not an
-exhaustive reference for the curses Application Programming Interface (API);
-that role is filled by the curses manual pages. Rather, it is intended to help
-C programmers ease into using the package.
+The curses package is a subroutine library for terminal-independent
+screen-painting and input-event handling which presents a high level
+screen model to the programmer, hiding differences between terminal
+types and doing automatic optimization of output to change one screen
+full of text into another. Curses uses terminfo, which is a database
+format that can describe the capabilities of thousands of different
+terminals.
 
-This document is aimed at C applications programmers not yet specifically
-familiar with ncurses. If you are already an experienced curses programmer, you
-should nevertheless read the sections on Mouse Interfacing, Debugging,
-Compatibility with Older Versions, and Hints, Tips, and Tricks. These will
-bring you up to speed on the special features and quirks of the ncurses
-implementation. If you are not so experienced, keep reading.
-
-The curses package is a subroutine library for terminal-independent screen-painting and input-event handling which presents a high level screen model to the programmer, hiding differences between terminal types and doing automatic optimization of output to change one screen full of text into another. Curses uses terminfo, which is a database format that can describe the capabilities of thousands of different terminals.
+Press any key to exit.
 EOF;
 
 $ncurses = new Ncurses();
+$ncurses->cursorVisibility(false);
 
-$widget0 = new Widget\Text($text, new Rect(0, 0, 8, 72));
-$widget1 = new Widget\Text($text, new Rect(15, 0, null, 72));
-$widget2 = new Widget\Text($text, new Rect(30, 0, null, null));
+$widget = new Widget\Text($text, new Rect(0, 0));
 
 $window = new Window(new Rect(0, 0, $ncurses->rows, $ncurses->cols));
-$window->widgets->add($widget0);
-$window->widgets->add($widget1);
-$window->widgets->add($widget2);
+$window->widgets->add($widget);
 $window->refresh();
 
 $ncurses->refresh();
