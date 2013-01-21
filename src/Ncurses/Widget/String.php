e81ee1f3bf2ee2f30e76b6c970e790df52bcb6fe
Juan M <jm@guide42.com>
2013-01-21 17:37:20 +0100

    Add String Widget.

---
+++ src/Ncurses/Widget/String.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ncurses\Widget;
+
+use Ncurses\Widget\Widget;
+use Ncurses\Util\Rect;
+
+/**
+ * String Widget
+ */
+class String extends Widget
+{
+    protected $string;
+
+    /**
+     * Constructor.
+     *
+     * @param string $string
+     * @param \Ncurses\Util\Rect $rect
+     */
+    public function __construct($string, Rect $rect)
+    {
+        parent::__construct($rect);
+
+        $this->string = $string;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Ncurses\Widget\Widget::draw()
+     */
+    public function draw()
+    {
+        ncurses_mvwaddstr($this->getWindow()->getResource(),
+            $this->rect->top,
+            $this->rect->left,
+            $this->label
+        );
+    }
+}
